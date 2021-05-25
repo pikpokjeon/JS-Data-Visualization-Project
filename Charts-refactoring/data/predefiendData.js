94de6aa7b979825b69601367b332bf83f9b64999
pikpokjeon <pikpokjeon@gmail.com>
2021-05-25 13:56:44 +0900

    feature: set symbol tagName to store message store

---
+++ Charts-refactoring/data/predefiendData.js
@@ -0,0 +1,32 @@
+
+const predefinedData =
+[
+    {
+        topic: 'userInputs',
+        data: {
+            // [Symbol.toStringTag] : 'userInputs',
+            w: 1500,
+            d: [0, 230, ],
+            d_label: [2010, 2011],
+            d_memo: [1, 1],
+            lineType: 'default',
+        },
+        
+        subs: []
+    },
+    {
+        topic: 'chartEvent',
+        data: {
+            // [Symbol.toStringTag] : 'chartEvent',
+            lastIdx: -1,
+            selectedIdx: { start: -1, end: -1 }
+
+        },
+        subs: []
+
+    }
+
+
+]
+
+export { predefinedData }
