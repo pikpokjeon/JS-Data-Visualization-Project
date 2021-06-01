9f1d601172565a62a7ced516802d4251be13f364
pikpokjeon <pikpokjeon@gmail.com>
2021-06-02 00:23:49 +0900

    refactor: seperate functions by its role for initializing, identify svg elms by $

--- Charts/main.js
+++ Charts/main.js
@@ -14,14 +14,22 @@ import { _id, _name, appendAll, inputData, copyParams } from './helper.js'
 const initSetPathGroup = (props, Use) => (w, d) =>
 {
     const _ = Use(props)
-    const lineType = onChangeLineType(props, Use, 'line')()
-    const { stop0, stop1, stop2, stop3, fill, fillG, fillBG, frame, fillPath, defs, path, pathShadow, blur, lineShadow } = _.initPathSVG
+    const { lineType } = _.inputStore
+    const { stop0, stop1, stop2, stop3, fill, fillG, fillBG, frame, fillPath, defs, path, pathShadow, blur, lineShadow } = _.$.initPathSVG
 
-    console.log( _.initPathSVG)
     appendAll({ stop0, stop1, stop2, stop3 }).to(fill)
 
     props = [...props, w, d]
     updatePathGroup(props, Use)(lineType)
+
+
+    // appendTree 를 만들어 구조를 작성해서 확장 하도록
+
+    // appendTree(
+    //     _.$.initSVG['group'],
+    //     { defs, fillG, path, pathShadow }
+    // )
+
 
     appendAll({ blur }).to(lineShadow)
     appendAll({ fillPath }).to(frame)
@@ -31,17 +39,51 @@ const initSetPathGroup = (props, Use) => (w, d) =>
     updateAttr(pathShadow, {
         filter: 'url(#lineShadow)', style: `stroke: #373205, stroke-width:10`, opacity: 1})
     appendAll({ fillBG }).to(fillG)
-    appendAll({ defs, fillG, path, pathShadow }).to(_.initSVG['group'])
+    appendAll({ defs, fillG, path, pathShadow }).to(_.$.initSVG['group'])
 
 }
 
 
 
-const initSVGList = genSvgList('singleSVG').setID(svgIdList)
-    initSVGList[Symbol.toStringTag] = 'initSVG'
+const initSVGLists = (idList, list) =>  list.reduce((obj, cur) =>
+    {
+        const [svgGroup, groupName] = [cur.from, cur.as]
+    
+        const initList = genSvgList(`${svgGroup}`).setID(idList)
+        initList[Symbol.toStringTag] = groupName
 
-const initPathList = genSvgList('pathGroup').setID(svgIdList)
-    initPathList[Symbol.toStringTag] = 'initPathSVG'
+        Object.assign( obj, { [groupName]: initList} )
+
+        return obj
+    }, {})
+
+
+
+const initSVGElements = (obj) => Object.entries(obj).reduce((elStore, cur) =>
+{
+    const [name, list] = [cur[0], cur[1]]
+
+    const tempEls = genSvgFromList(list, inputData(_id('width')), inputData(_id('data-list')),).named(name)
+    elStore[Symbol.toStringTag] = '$'
+
+    Object.assign( elStore,  { [name]: {...tempEls} } )
+
+    return elStore
+},{})
+
+
+
+const initSVGListObj = initSVGLists(svgIdList, [
+    {
+        from: 'singleSVG',
+        as: 'initSVG'
+    },
+    {
+        from: 'pathGroup',
+        as: 'initPathSVG'
+    }
+])
+
 
 
 
@@ -66,8 +108,7 @@ const initParams = [
     updateTooltip,
     genSvgList,
     genPath,
-    genSvgFromList(initSVGList, inputData(_id('width')), inputData(_id('data-list')),).named('initSVG'),
-    genSvgFromList(initPathList, inputData(_id('width')), inputData(_id('data-list')),).named('initPathSVG'),
+    initSVGElements(initSVGListObj),
     initSetPathGroup,
     genSvgFromList,
     DOMEventAttr,
@@ -81,19 +122,18 @@ const initParams = [
 const init = (props, Use) =>
 {
     const _ = Use(props)
-
     const [d, w] = [inputData(_id('data-list')), inputData(_id('width'))]
     const initData = [0, 230, 120, -450, -200, 1600, 0, 600, -1500, 200, 0, -1200, -800, 800, 0]
     Publish(_.inputStore, { w: w, d: initData, dLabel:initData.map((_, i) => 2010 + i) })
 
 
-    const [svgArea, svg] = [_id('svg-area'), _.initSVG['svg']]
+    const [svgArea, svg] = [_id('svg-area'), _.$.initSVG['svg']]
 
     _id('data-list').value = initData.join(',')
     svgArea.appendChild(svg)
 
-    delete (_.initSVG['svg'])
-    const onMoveprops = [updateAttr, genSize, _id, _.initSVG, _.initPathSVG, inputData, setEvents, Publish, chartStore, inputStore]
+    delete (_.$.initSVG['svg'])
+    const onMoveprops = [updateAttr, genSize, _id, _.$, _.$.initSVG, _.$.initPathSVG, inputData, setEvents, Publish, chartStore, inputStore]
     const mouseOn = () => { svg.addEventListener('mousemove', onMove(onMoveprops, Use)) }
     const mouseOut = () => { svg.removeEventListener('mousemove', onMove(onMoveprops, Use)) }
 
@@ -106,8 +146,8 @@ const init = (props, Use) =>
 
     initSetPathGroup(props, Use)(w, initData)
     setEvents(props, Use).addAll(_.DOMEventAttr)
-    appendAll(_.initSVG).to(svg)
-    _.initSVG = { svg, ..._.initSVG }
+    appendAll(_.$.initSVG).to(svg)
+    _.$.initSVG = { svg, ..._.$.initSVG }
     updateTooltip(props, Use)(w, initData, initData.map((_, i) => 2010 + i))
 
 
