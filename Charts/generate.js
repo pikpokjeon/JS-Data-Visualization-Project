b842c54b536b191e6321d7e440f6fc9a5bfb205e
pikpokjeon <pikpokjeon@gmail.com>
2022-01-09 17:55:18 +0900

    style : 폰트 Roboto적용

--- Charts/generate.js
+++ Charts/generate.js
@@ -71,158 +71,161 @@ const genAttr = (w, d, i, v) =>
 
     }
     const list = {
-        g: { width: w, height: h, style: 'overflow:visible' },
-        gBox: {
-        },
-        path: {
-            stroke: color.default, fill: "transparent", strokeWidth: 10
-        },
-        focusLine: {
-            'attributeName': 'stroke-width',
-            'attributeType': 'XML',
-            'values': '3;1;3;1',
-            'dur': '2s',
-            'repeatCount': '5',
-        },
-        moveX: {
-            x1: s.x(i),
-            x2: s.x(i),
-        },
-        moveY: {
-            y1: s.y(i),
-            y2: s.y(i),
-        },
-        eventArea: {
-            ...svg,
-            style: style.opacity(0.2)
-            // fill: color.bg,
-        },
-        lineH: {
-            x1: 0,
-            y1: s.y(v),
-            x2: s.width,
-            y2: s.y(v),
-            style: style.line + style.opacity(0.3),
-        },
-        lineV: {
-            x1: s.x(i),
-            y1: -h,
-            x2: s.x(i),
-            y2: h * 2,
-            style: style.line,
-        },
-        borderLine: {
-            x1: s.x(i),
-            y1: -h,
-            x2: s.x(i),
-            y2: h * 2,
-            style: style.line + " stroke-dasharray:5,5;" + style.opacity(0.5),
-        },
-        label: {
-            x: s.x(i),
-            y: h - 30,
-            fill: color.default,
-            'dominant-baseline': 'start',
-            'text-anchor': 'middle',
-        },
-        dataText: {
-            x: 40,
-            y: s.y(v),
-            fill: color.default,
-            'dominant-baseline': 'end',
-            'text-anchor': 'middle',
-        },
-        plot: {
-            cx: s.x(i),
-            cy: s.y(v),
-            r: 5,
-            fill: "white"
-        },
-        stop0: { offset: '0%', style: 'stop-color: #9bffc9; stop-opacity: 1' },
-        stop1: { offset: '30%', style: 'stop-color: white; stop-opacity: 0.8' },
-        stop2: { offset: '50%', style: 'stop-color: #00f0ff; stop-opacity: 0.2' },
-        stop3: { offset: '100%', style: 'stop-color:#4b00ff; stop-opacity: 0' },
-        linearGradient: {  //fill
-            x1: '0%',
-            x2: '0%',
-            y1: '0%',
-            y2: '100%',
-        },
-        fillG: {
-            'clip-path': 'url(#frame)'
-        },
-        fillBG: {
-            x: 0,
-            y: 700,
-            width: w,
-            height: 700,
-            fill: 'url(#fill)'
-        },
-        clipPath: { // frame
-        },
-        fillPath: {
-            // filter: 'url(#lineShadow)',
-            fill: color.default,
-        },
-        defs: {
-        },
-        filter: {
-            x: '0',
-            y: '-10',
-            width: '14',
-            height: '14',
-
-        },
-        feGaussianBlur: {
-            stdDeviation: "2.5"
-        },
-        msgTitle: {
-            x: 30,
-            y: 30,
-            fill: color.default,
-            'dominant-baseline': 'start',
-            'text-anchor': 'start',
-        },
-        msgValue: {
-            x: 70,
-            y: 30,
-            fill: color.default,
-            'dominant-baseline': 'start',
-            'text-anchor': 'start',
-        },
-        msgBox: {
-            x: 0,
-            y: 0,
-            width: s.msgBox.width,
-            height: s.msgBox.height,
-            fill: color.bg,
-            style: 'opacity:0.65;stroke:black;stroke-width:1'
-
-        },
-        msgShadow: {
-            x: 0,
-            y: 0,
-            width: s.msgBox.width,
-            height: s.msgBox.height,
-            fill: 'black',
-            filter: 'url(#msgFilter)',
-            style: 'opacity:0.4',
-            stroke: 1,
-        },
-        msgGroup: {
-            transform: `translate(100,100)`
-        },
-        bar: {
-            x: s.x(i),
-            y: s.y(v),
-            width: 7,
-            height: 10,
-            fill: color.default,
-            rx: 10,
-            ry: 10,
-        }
-
-    }
+      g: { width: w, height: h, style: "overflow:visible" },
+      gBox: {},
+      path: {
+        stroke: color.default,
+        fill: "transparent",
+        strokeWidth: 10,
+      },
+      focusLine: {
+        attributeName: "stroke-width",
+        attributeType: "XML",
+        values: "3;1;3;1",
+        dur: "2s",
+        repeatCount: "5",
+      },
+      moveX: {
+        x1: s.x(i),
+        x2: s.x(i),
+      },
+      moveY: {
+        y1: s.y(i),
+        y2: s.y(i),
+      },
+      eventArea: {
+        ...svg,
+        style: style.opacity(0.2),
+        // fill: color.bg,
+      },
+      lineH: {
+        x1: 0,
+        y1: s.y(v),
+        x2: s.width,
+        y2: s.y(v),
+        style: style.line + style.opacity(0.3),
+      },
+      lineV: {
+        x1: s.x(i),
+        y1: -h,
+        x2: s.x(i),
+        y2: h * 2,
+        style: style.line,
+      },
+      borderLine: {
+        x1: s.x(i),
+        y1: -h,
+        x2: s.x(i),
+        y2: h * 2,
+        style: style.line + " stroke-dasharray:5,5;" + style.opacity(0.5),
+      },
+      label: {
+        x: s.x(i),
+        y: h - 30,
+        fill: color.default,
+        "dominant-baseline": "start",
+        "text-anchor": "middle",
+        "font-family": "Roboto",
+      },
+      dataText: {
+        x: 40,
+        y: s.y(v),
+        fill: color.default,
+        "dominant-baseline": "end",
+        "text-anchor": "middle",
+        "font-family": "Roboto",
+      },
+      plot: {
+        cx: s.x(i),
+        cy: s.y(v),
+        r: 5,
+        fill: "white",
+      },
+      stop0: { offset: "0%", style: "stop-color: #9bffc9; stop-opacity: 1" },
+      stop1: { offset: "30%", style: "stop-color: white; stop-opacity: 0.8" },
+      stop2: { offset: "50%", style: "stop-color: #00f0ff; stop-opacity: 0.2" },
+      stop3: { offset: "100%", style: "stop-color:#4b00ff; stop-opacity: 0" },
+      linearGradient: {
+        //fill
+        x1: "0%",
+        x2: "0%",
+        y1: "0%",
+        y2: "100%",
+      },
+      fillG: {
+        "clip-path": "url(#frame)",
+      },
+      fillBG: {
+        x: 0,
+        y: 700,
+        width: w,
+        height: 700,
+        fill: "url(#fill)",
+      },
+      clipPath: {
+        // frame
+      },
+      fillPath: {
+        // filter: 'url(#lineShadow)',
+        fill: color.default,
+      },
+      defs: {},
+      filter: {
+        x: "0",
+        y: "-10",
+        width: "14",
+        height: "14",
+      },
+      feGaussianBlur: {
+        stdDeviation: "2.5",
+      },
+      msgTitle: {
+        x: 30,
+        y: 30,
+        fill: color.default,
+        "dominant-baseline": "start",
+        "text-anchor": "start",
+        "font-family": "Roboto",
+      },
+      msgValue: {
+        x: 70,
+        y: 30,
+        fill: color.default,
+        "dominant-baseline": "start",
+        "text-anchor": "start",
+        "font-family": "Roboto",
+      },
+      msgBox: {
+        x: 0,
+        y: 0,
+        width: s.msgBox.width,
+        height: s.msgBox.height,
+        fill: color.bg,
+        style: "opacity:0.65;stroke:black;stroke-width:1",
+      },
+      msgShadow: {
+        x: 0,
+        y: 0,
+        width: s.msgBox.width,
+        height: s.msgBox.height,
+        fill: "black",
+        filter: "url(#msgFilter)",
+        style: "opacity:0.4",
+        stroke: 1,
+      },
+      msgGroup: {
+        transform: `translate(100,100)`,
+      },
+      bar: {
+        x: s.x(i),
+        y: s.y(v),
+        width: 7,
+        height: 10,
+        fill: color.default,
+        rx: 10,
+        ry: 10,
+      },
+    };
     return { svg, color, style, ...list }
 }
 
